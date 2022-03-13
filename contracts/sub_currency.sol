95a6d2d7d779a4b67e82e7e0fba71b294fc939eb
"ejyang" <"trustnloveu@gmail.com">
2022-03-13 17:14:59 +0900

    create custom token ( + revert, event, error)

---
+++ contracts/sub_currency.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+
+pragma solidity ^0.8.4;
+
+contract Coin {
+
+    // The keyword "public" makes variables accessible from other contracts
+    address public minter;
+    mapping (address => uint) public balances;
+
+     /* 
+        `Events` allows clients to react to specific contract changes you declare
+        
+        Event is an inheritable member of a contract. An event is emitted, it stores the arguments passed in transaction logs. 
+        These logs are stored on blockchain and are accessible using address of the contract till the contract is present on the blockchain. 
+     */ 
+    event Sent(address from, address to, uint amount);
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    // Send an amount of newly created coins to an address
+    // Can only be called by the contract creator
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter);
+
+        balances[receiver] += amount;
+    }
+
+    // `Error` allows you to provide information about why an operation failed. They are returned to the caller of the function.
+    error InsufficientBalance(uint requested, uint available);
+
+    // Send an amount of existing coins from any caller to an address
+    function send(address receiver, uint amount) public {
+        
+        if (amount > balances[msg.sender]) {
+            revert InsufficientBalance(
+                {
+                    requested: amount,
+                    available: balances[msg.sender]
+                }
+            );
+        }
+
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
